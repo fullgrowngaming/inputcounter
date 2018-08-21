08079e969924a400bbb73c8e7d2200f56a13a4ce
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2018-08-21 02:22:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ while 1:
     b = str(bytes(data, 'utf-8'))
     c = b.strip("b'")
     d = list(c)
-    buttons = []
     if len(d) == 9:
         pass
     elif len(d) != 9:
         buttons = {'Start': d[15], 'Y': d[19], 'X': d[23], 'B': d[27], 'A': d[31], 'L': d[39], 'R': d[43], 'Z': d[47],
                    'D-Up': d[51], 'D-Down': d[55], 'D-Right': d[59], 'D-Left': d[63]}
 
-    for button in buttons:
-        if buttons[button] == '1':
-            print(button)
+        for button in buttons:
+            if buttons[button] == '1':
+                print(button)
 
 ws.close()
