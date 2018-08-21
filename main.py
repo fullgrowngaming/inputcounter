72b8928b879c18556ef88fca26579e430cf9c04e
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2018-08-21 01:13:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#I know this is super hacky and doesn't count inputs yet AND doesn't handle multiple buttons at the same time AND
-#never breaks out of the loop, but I'm proud to have gotten this far fggYay
+#http://nintendowispy.keepdream.in/?theme=Kylf&websockport=18881&websocketserver=NintendoWiSpy.local&inputdelay=275
 
 from websocket import *
 
@@ -10,32 +9,15 @@ while 1:
     b = str(bytes(data, 'utf-8'))
     c = b.strip("b'")
     d = list(c)
+    buttons = []
     if len(d) == 9:
         pass
     elif len(d) != 9:
-        if d[15] == '1':
-            print('Start')
-        elif d[19] == '1':
-            print('Y')
-        elif d[23] == '1':
-            print('X')
-        elif d[27] == '1':
-            print('B')
-        elif d[31] == '1':
-            print('A')
-        elif d[39] == '1':
-            print('L')
-        elif d[43] == '1':
-            print('R')
-        elif d[47] == '1':
-            print('Z')
-        elif d[51] == '1':
-            print('D-Up')
-        elif d[55] == '1':
-            print('D-Down')
-        elif d[59] == '1':
-            print('D-Right')
-        elif d[63] == '1':
-            print('D-Left')
+        buttons = {'Start': d[15], 'Y': d[19], 'X': d[23], 'B': d[27], 'A': d[31], 'L': d[39], 'R': d[43], 'Z': d[47],
+                   'D-Up': d[51], 'D-Down': d[55], 'D-Right': d[59], 'D-Left': d[63]}
+
+    for button in buttons:
+        if buttons[button] == '1':
+            print(button)
 
 ws.close()
